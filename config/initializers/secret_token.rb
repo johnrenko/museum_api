f4c0983950e73b3016772dfc85c9440081c659f2
johnrenko <vishnou@gmail.com>
2015-08-13 15:55:12 +1000

    Initialises the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MuseumApi::Application.config.secret_key_base = 'ca4def58ed8cf999b813d172225e1538e2e0d42a2e239a2cab7aa48c9114b5e88dc17f83bd7d90d9b1ccc9254d3557e29debe1c3c45c02c2b1ab82d7a91b0237'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MuseumApi::Application.config.session_store :cookie_store, key: '_Museum_api_session'
