1da4b6d02dd6ff461d692081202d6b9505007f9e
johnrenko <vishnou@gmail.com>
2015-08-13 21:59:13 +1000

    latest update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
-MuseumApi::Application.routes.draw do
-  namespace :api, default: { format: :json} do
-    constraints: {subdomain: 'api'}, path: '/' do
-      
+require 'api_constraints'
+
+MarketPlaceApi::Application.routes.draw do
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+    end
   end
 end
